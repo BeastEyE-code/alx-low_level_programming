4dabac9482429c9d880370a0a9bce11daa81a38d
Owolabi Muiz <beasteye.official@gmail.com>
2022-06-07 12:55:54 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+include "_putchar.h"
+
+/**
+ *print putchar
+ *Return: 0
+ */
+
+int main(void)
+{
+char c[10] = "Putchar";
+int i = 0;
+while (i<9)
+{
+_putchar('\n');
+}
+_putchar('\n');
+return (0);
+}
